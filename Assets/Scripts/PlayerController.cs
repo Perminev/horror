383138d09fdd2bc7792b5ab7d6c28c132163fe07
Perminev <gperminev@gmail.com>
2024-07-10 16:00:17 +0200

    Добавил возможность перемещения
    Добавил возможность перемещения персонажа

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,25 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     Vector3 direction;
     
+    
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = movementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        direction = transform.TransformDirection(direction);
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime);
     }
 }
