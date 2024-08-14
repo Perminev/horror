fef07821f3e9bc47567062dba857d91b2a2018aa
Perminev <gperminev@gmail.com>
2024-08-14 11:07:49 +0300

    Added many changes

---
+++ Assets/SAFING Key Door/PlayerKey.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerKey : MonoBehaviour
+{
+    [SerializeField] KeyCode PickUp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if(Physics.Raycast(ray, out hit, 3))
+        {
+            if(hit.collider.tag == "Key")
+            {
+                if (Input.GetKeyDown(PickUp))
+                    {
+                    hit.collider.gameObject.GetComponent<KeyEvent>().UnlockDoor();
+                }
+            }
+            if (hit.collider.tag == "Door")
+            {
+                if (Input.GetKeyDown(PickUp))
+                {
+                    hit.collider.gameObject.GetComponent<DoorEvent>().TryOpen();
+                }
+            }
+        }
+    }
+}
